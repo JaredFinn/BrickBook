42435d4ab25d140048fbe7f8bc48deff5b7d55f5
Chappellm18 <Chapps360@gmail.com>
2021-05-31 18:02:47 -0400

    Bruh

---
+++ brickbook/src/components/MakePlan.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="Make-Plan">
+    Make Plan
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MakePlan',
+  data () {
+    return {
+      msg: 'Make plan here'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
