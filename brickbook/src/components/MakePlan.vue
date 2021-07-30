09515350eb7c2013752089fead4c139528b6906f
Chappellm18 <Chapps360@gmail.com>
2021-07-29 20:42:55 -0400

    wht rework -needs styling

--- brickbook/src/components/MakePlan.vue
+++ brickbook/src/components/MakePlan.vue
@@ -1,18 +1,23 @@
 <template>
-  <div class="Make-Plan">
-    Make Plan
-  <div></div>
-  </div>
+  <section>
+
+        <b-field>
+            <b-input placeholder="Location" type="text"></b-input>
+        </b-field>
+
+        <b-field>
+            <b-input type="textarea"
+                minlength="10"
+                maxlength="100"
+                placeholder="Whats happening there...">
+            </b-input>
+        </b-field>
+    </section>
 </template>
 
 <script>
 export default {
   name: 'MakePlan',
-  data () {
-    return {
-      msg: 'Make plan here'
-    }
-  }
 }
 </script>
 
--- brickbook/src/components/WhatsHappeningTn.vue
+++ brickbook/src/components/WhatsHappeningTn.vue
@@ -1,65 +1,52 @@
 <template>
   <div class="WhatsHappeningTn">
-      <a class="panel-block is-active">
-            <div class="make-plan">               
-                    <div id="but">
-                        <button class="button is-small is-fullwidth" v-on:click="CreatePlan">Small</button>
-                    </div>
-                
+    <a class="panel-block is-active">
+      <div class="current-plan">
+        <!-- area for current plans -->
+        <div class="plan">
+            planss
+        </div>
+        <div class="make-plan">
+          <b-collapse :open="false" aria-id="contentIdForA11y1">
+            <template #trigger>
+              <b-button
+                label="Make Plan"
+                type="is-primary"
+                aria-controls="contentIdForA11y1"
+              />
+            </template>
+            <div class="notification">
+              <div class="content">
+                <MakePlan />
+              </div>
             </div>
-            <div class="current-plan">
-
-            </div>
-        </a>
+          </b-collapse>
+        </div>
+      </div>
+    </a>
   </div>
 </template>
 
 <script>
-//import Vue from 'vue'
-//import MakePlan from '../components/MakePlan';
-//Vue.component('MakePlan', MakePlan)
-
-
+import MakePlan from "../components/MakePlan.vue";
 export default {
-    name: 'but',
-    methods: {
-        CreatePlan: function (event) {
-        console.log('test clicked' + event)
-        }
-    }//,
-    //components: {
-    //    MakePlan
-   // }
-}
-
+  name: "but",
+  components: {
+    MakePlan,
+  },
+};
 </script>
 
 <style>
 .WhatsHappeningTn {
-    align-items: center;
-    padding-right: 5px;
-    padding-left: 5px;
-    height: 300px;
-    width: 500px;
-    border: 2px solid;
-    border-radius: 5px;
+  padding: 5px;
 }
 .make-plan {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 30px;
-    width: 300px;
-    border: 3px solid rgb(0, 30, 128); 
-    bottom: 0px;
+  border: 1px solid;
 }
 .current-plan {
-    border: 2px solid;
-    align-items: center;
-    border-radius: 5px;
-    height: 225px;
-    width: 350px;
-    padding-left: 25px;
-    padding-right: 25px;
+  height: 400px;
+  width: 100%;
+  border: 1px solid;
 }
 </style>
